20deaef75411333107a3672734b457d88d6363d9
Nikko Ong <nikko@docugami.com>
2024-07-20 03:00:24 -0400

    initial push

---
+++ background.js
@@ -0,0 +1,63 @@
+let extensionActive = false;
+
+
+// function to update badge
+function updateBadge(extensionActive) {
+  if (extensionActive) {
+    chrome.action.setBadgeText({text: '['});
+    chrome.action.setBadgeBackgroundColor({color: '#00FF00'}); // set badge to green
+  } else {
+    chrome.action.setBadgeText({ text: 'x' });
+    chrome.action.setBadgeBackgroundColor({ color: '#FF0000' }); // Red
+  }
+}
+
+// Load initial state from storage
+function loadState() {
+  chrome.storage.local.get(['extensionActive'], (result) => {
+    extensionActive = result.extensionActive || false;
+    updateBadge(extensionActive);
+  });
+}
+
+// Save state to storage
+function saveState() {
+  chrome.storage.local.set({extensionActive});
+}
+
+// inject content script if not already injected
+function sendMessage(tab) {
+    chrome.tabs.sendMessage(tab.id, {action: 'toggleExtension', extensionActive: extensionActive}, (response) => {
+      if (chrome.runtime.lastError) {
+        console.log("Error sending current state to content script: ", chrome.runtime.lastError.message);
+      } else {
+      console.log("Current state sent to content script, response: ", response);
+      }
+    }); 
+  }
+
+// Get initial state  
+loadState();
+
+// Toggling logic with action button 
+chrome.action.onClicked.addListener((tab) => {
+  console.log("Icon clicked!"); // Log to verify the icon click
+  
+  // toggle extension state
+  extensionActive = !extensionActive;
+  updateBadge(extensionActive);
+  saveState(); // Save new state to storage
+
+  // Notify content script
+  sendMessage(tab);
+});
+
+// Listener for messages from content scripts
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'getState') {
+    chrome.storage.local.get(['extensionActive'], (result) => {
+      sendResponse({extensionActive: result.extensionActive || false});
+    });
+    return true; // Keep the message channel open for async response
+  }
+});
---
+++ content.js
@@ -0,0 +1,87 @@
+
+console.log("Extension loaded.");
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === 'toggleExtension') {
+        extensionActive = request.extensionActive;
+        console.log(`Extension is now ${request.extensionActive ? 'active' : 'inactive'}`);
+        sendResponse({ status: 'success', extensionActive: extensionActive});
+    }
+  });
+
+// Wrap the selected text
+function wrapSelectedText(key, activeElement,start, end) {
+    try
+    {
+        if (activeElement && (activeElement.tagName === 'TEXTAREA' || (activeElement.tagName === 'INPUT' && activeElement.type === 'text') || (activeElement.tagName === 'DIV' && activeElement.isContentEditable))) 
+        {       
+            const selectedText = activeElement.value.substring(start,end)
+            if (selectedText) 
+                {
+                    let wrappedText;
+                    switch (key) 
+                    {
+                        case '"':
+                            wrappedText = `"${selectedText}"`;
+                            break;
+                        case '[':
+                            wrappedText = `[${selectedText}]()`;
+                            break;
+                        case '{':
+                            wrappedText = `{${selectedText}}`;
+                            break;
+                        case '*':
+                            wrappedText = `*${selectedText}*`;
+                            break;
+                        case '_':
+                            wrappedText = `_${selectedText}_`;
+                            break;
+                        case '$':
+                            wrappedText = `$${selectedText}$`;
+                            break;
+                        case '`':
+                            wrappedText = `\`${selectedText}\``;
+                            break;
+                        case '=':
+                            wrappedText = `==${selectedText}==`;
+                            break;
+                        case '~':
+                            wrappedText = `~~${selectedText}~~`;
+                            break;
+                        }
+                    activeElement.setRangeText(wrappedText, start, end, 'end');
+                    if (key === '[') {
+                        const modCursorPos = start + wrappedText.length - 1;
+                        // set cursor position to be inside the parentheses
+                        activeElement.setSelectionRange(modCursorPos,modCursorPos)
+                    }
+                }
+        }
+    } catch (error)
+    {
+        console.error("Error wrapping selected text: ", error);
+    }
+}
+
+// Listen for keydown events when the extension is active
+document.addEventListener('keydown', function(event) {
+    if (extensionActive && ['"', '[', '{', '*', '_', '$', '`', '=', '~'].includes(event.key)) {
+        const activeElement = document.activeElement;
+        const start = activeElement.selectionStart;
+        const end = activeElement.selectionEnd;
+        // Exit function if no text is selected
+        if (start == end) {
+            return;
+        }
+        event.preventDefault(); // Prevent the default key action
+        wrapSelectedText(event.key, activeElement, start, end);
+    }
+  });
+
+// Request initial state from background script
+chrome.runtime.sendMessage({action: 'getState'}, (response) => {
+if (response) {
+    extensionActive = response.extensionActive;
+    console.log(`Extension state from storage is set to ${extensionActive}`)
+}
+});
